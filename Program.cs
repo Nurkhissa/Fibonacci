1596cdbe336051f8462af374c4b69a83ac2ce3c0
Nurkhissa <45072957+Nurkhissa@users.noreply.github.com>
2024-02-13 23:50:36 +0500

    Upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+int a = 0;
+int b = 1;
+for (int i = 0; i < n; i++)
+ {
+    
+    int temp = a;
+    a = b;
+    b = temp + b;
+    if(a<n)
+        System.Console.WriteLine(a);
+    else break;
+    
+}
+
